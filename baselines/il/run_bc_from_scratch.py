706ef53373151d4fc1e518f17637eab78f9a7285
kevinjeon <kevinjeon119@gmail.com>
2024-12-18 11:23:45 +0900

    Modify dataset

--- baselines/il/run_bc_from_scratch.py
+++ baselines/il/run_bc_from_scratch.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 import torch
 from torch.optim import Adam
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 import os, sys, torch
 sys.path.append(os.getcwd())
 import wandb, yaml, argparse, shutil
@@ -49,12 +49,15 @@ class ExpertDataset(torch.utils.data.Dataset):
         self.obs = obs
         obs_pad = np.zeros((obs.shape[0], rollout_len - 1, *obs.shape[2:]), dtype=np.float32)
         self.obs = np.concatenate([obs_pad, self.obs], axis=1)
+        self.masks = 1 - masks
+        dead_masks_pad = np.ones((self.masks.shape[0], rollout_len - 1, *self.masks.shape[2:]), dtype=np.float32)
+        self.masks = np.concatenate([dead_masks_pad, self.masks], axis=1)
+
         self.road_mask = road_mask
         road_mask_pad = np.zeros((road_mask.shape[0], rollout_len - 1, *road_mask.shape[2:]), dtype=np.float32)
         self.road_mask = np.concatenate([road_mask_pad, self.road_mask], axis=1)
         
         self.actions = actions
-        self.masks = 1 - masks
         self.other_info = other_info
         self.num_timestep = 1 if len(obs.shape) == 2 else obs.shape[1] - rollout_len - pred_len + 2
         self.rollout_len = rollout_len
@@ -66,13 +69,21 @@ class ExpertDataset(torch.utils.data.Dataset):
         self.partner_mask = np.concatenate([partner_mask_pad, self.partner_mask], axis=1)
         if self.masks is not None:
             self.use_mask = True
-        
+        self.valid_indices = self._compute_valid_indices()
         self.full_var = ['obs', 'actions', 'masks', 'partner_mask', 'road_mask'] # todo: add other info
 
     def __len__(self):
-        return len(self.obs) * self.num_timestep
+        return len(self.valid_indices)
 
+    def _compute_valid_indices(self):
+        N, T = self.masks.shape
+        valid_time = np.arange(T - (self.rollout_len + self.pred_len - 2))
+        valid_idx1, valid_idx2 = np.where(self.masks[:, valid_time + self.rollout_len + self.pred_len - 2] == 1)
+        valid_idx2 = valid_time[valid_idx2]
+        return list(zip(valid_idx1, valid_idx2))
+    
     def __getitem__(self, idx):
+        idx1, idx2 = self.valid_indices[idx]
         # row, column -> 
         batch = ()
         if self.num_timestep > 1:
@@ -81,11 +92,11 @@ class ExpertDataset(torch.utils.data.Dataset):
             for var_name in self.full_var:
                 if self.__dict__[var_name] is not None:
                     if var_name in ['obs', 'road_mask', 'partner_mask']:
-                        data = self.__dict__[var_name][idx1, idx2 + self.rollout_len - 1:idx2 + 2 * self.rollout_len - 1] # idx 1 -> (0, 1 + 10 -1) -> (0, 10) start with first timestep
+                        data = self.__dict__[var_name][idx1, idx2:idx2 + self.rollout_len] # idx 0 -> (0, 0:10) -> (0, 9) end with first timestep
                     elif var_name == 'actions':
-                        data = self.__dict__[var_name][idx1, idx2 + self.rollout_len - 1:idx2 + self.rollout_len + self.pred_len - 1]
+                        data = self.__dict__[var_name][idx1, idx2:idx2 + self.pred_len] # idx 0 -> (0, 0:5) -> start with first timestep
                     elif var_name == 'masks':
-                        data = self.__dict__[var_name][idx1 ,idx2 + self.rollout_len + self.pred_len - 2]
+                        data = self.__dict__[var_name][idx1 ,idx2 + self.rollout_len + self.pred_len - 2] # idx 0 -> (0, 10 + 5 - 2) -> (0, 13) & padding = 9 -> end with last action timestep
                     else:
                         raise ValueError(f"Not in data {self.full_var}. Your input is {var_name}")
                     batch = batch + (data, )
@@ -157,15 +168,16 @@ if __name__ == "__main__":
     eval_expert_masks = np.concatenate(eval_expert_masks) if len(eval_expert_masks) > 0 else None
     eval_other_info = np.concatenate(eval_other_info) if len(eval_other_info) > 0 else None
     eval_road_mask = np.concatenate(eval_road_mask) if len(eval_road_mask) > 0 else None
-    dataset_len = len(train_expert_obs)
     num_cpus = os.cpu_count()
-    # DataLoader with multiple workers
-    expert_data_loader = DataLoader(
-        ExpertDataset(
+    train_dataset = ExpertDataset(
             train_expert_obs, train_expert_actions, train_expert_masks,
             other_info=train_other_info, road_mask=train_road_mask,
             rollout_len=args.rollout_len, pred_len=args.pred_len
-        ),
+        )
+    dataset_len = len(train_dataset)
+    # DataLoader with multiple workers
+    expert_data_loader = DataLoader(
+        train_dataset,
         batch_size=exp_config.batch_size,
         shuffle=True,
         num_workers=int(num_cpus / 2),
